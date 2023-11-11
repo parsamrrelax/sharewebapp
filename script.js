109907be5270ed2c53c948cb564cc196b99371f0
Parsalp <parsa.osunlou1998@gmail.com>
2023-11-12 02:30:43 +0330

    index change

--- script.js
+++ script.js
@@ -100,21 +100,38 @@ function displayResult() {
 
     // Return the results as JSON
     console.log(getResultsAsJson());
-    sendToTelegram();
 }
+function loadTelegramWebAppScript() {
+    const script = document.createElement('script');
+    script.src = 'https://telegram.org/js/telegram-web-app.js';
+    script.async = true;
+    script.onload = function () {
+        // The Telegram Web App script has been successfully loaded
+        console.log('Telegram Web App script has been loaded.');
+        // Add any further logic you need after the script is loaded
+    };
+    script.onerror = function () {
+        // An error occurred while loading the script
+        console.error('Error loading Telegram Web App script.');
+        // Handle the error or retry loading if necessary
+    };
+
+    document.head.appendChild(script);
+}
+
+// Call the function to load the Telegram Web App script
+loadTelegramWebAppScript();
 
 function getResultsAsJson() {
     const results = {
         people: people,
         items: items
     };
-    return JSON.stringify(results, null, 2);
-}
-
-function sendToTelegram() {
     Telegram.WebApp.ready();
     Telegram.WebApp.MainButton.setText('Price Calculate').show().onClick(function () {
-        Telegram.WebApp.sendData(getResultsAsJson());
+        const data = JSON.stringify(results, null, 2);
+        Telegram.WebApp.sendData(data);
         Telegram.WebApp.close();
     });
+    return JSON.stringify(results, null, 2);
 }
